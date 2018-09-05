0d992fab058ba93f1e42323ef44871f004d27c67
MainDuelo <mainduelo@gmail.com>
2018-09-05 10:00:07 -0300

    Added binary in calculator

--- src/main/java/br/com/calc/ConverteBinario.java
+++ src/main/java/br/com/calc/ConverteBinario.java
@@ -1,9 +1,26 @@
 package br.com.calc;
 
 public class ConverteBinario {
-	
-	public int converteParaBinario(int numberTest) {
-		return 10;
-	}
 
+	public int convertBinary(int number) {
+		if(number <1)
+			return 0;
+		
+		int leftover;
+		String binary = "";
+		while(number > 0) {
+			leftover = number % 2;
+			binary += leftover;
+			number = number / 2;
+		}		
+		return Integer.parseInt(stringReverse(binary));
+	}
+	
+	private String stringReverse(String text) {
+		String inverted = "";
+		for (int i = text.length() -1; i >= 0; i--)
+			inverted += text.charAt(i);
+			
+		return inverted;
+	}
 }
